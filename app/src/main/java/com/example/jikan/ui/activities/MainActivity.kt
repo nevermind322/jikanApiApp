bbac91166b88114b1bb3f0edb0d4cc96c0f2755e
nevermind322 <alexfilonov2015@yandex.ru>
2023-04-29 20:57:48 +0300

    delete AnimeViewModel

--- app/src/main/java/com/example/jikan/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/jikan/ui/activities/MainActivity.kt
@@ -1,48 +1,21 @@
 package com.example.jikan.ui.activities
 
 import android.os.Bundle
-import android.util.Log
-import android.view.MotionEvent
 import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.MotionEventCompat
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.NavHostFragment
-import com.example.jikan.*
 import com.example.jikan.data.AnimeInfo
-import com.example.jikan.data.datasources.JikanAnimeDataSource
-import com.example.jikan.data.repos.AnimeRepository
 import com.example.jikan.databinding.ActivityMainBinding
 import com.example.jikan.ui.fragments.HomeFragmentDirections
-import com.example.jikan.viewModels.AnimeInfoUiState
-import com.example.jikan.viewModels.AnimeViewModel
-import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
-    private val animeViewModel by viewModels<AnimeViewModel> {
-        AnimeViewModel.AnimeViewModelProviderFactory(AnimeRepository((JikanAnimeDataSource())))
-    }
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        animeViewModel.resetValue()
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                animeViewModel.uiState.collect {
-                    Log.i("nav", "collected $it")
-                    when (it) {
-                        is AnimeInfoUiState.Error -> showError(it.e)
-                        is AnimeInfoUiState.Success -> showAnimePage(it.info)
-                        else -> {}
-                    }
-                }
-            }
-        }
     }
 
     fun showAnimePage(info: AnimeInfo) {
